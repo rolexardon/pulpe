828b9457beeb73971f83c3764c13dd2599e364fd
rolexardon <rolando_ardon299@hotmail.com>
2014-10-09 16:10:16 -0600

    'changes'

--- inventario/urls.py
+++ inventario/urls.py
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('inventario.views',
     url(r'^ver/$', 'ver_productos', name='ver_productos'),
 	url(r'^ejecutar/compra$', 'post_compra', name='post_compra'),
-	
-=======
-from django.conf.urls import patterns, url
 
-urlpatterns = patterns('inventario.views',
-    url(r'^ver/$', 'ver_productos', name='ver_productos'),
-	url(r'^ejecutar/compra$', 'post_compra', name='post_compra'),
-	
->>>>>>> fa98660b45de57019fe6f40d7e8f076bce546715
 )
--- pulpe/views.py
+++ pulpe/views.py
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from inventario.models import producto
 
 def home(request):
 	productos = producto.objects.all()
-=======
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-from inventario.models import producto
-
-def home(request):
-	productos = producto.objects.all()
->>>>>>> fa98660b45de57019fe6f40d7e8f076bce546715
 	return render_to_response('home.html',{'productos':productos},context_instance=RequestContext(request))
