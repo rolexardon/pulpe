8ebab7fa29671d96117c43ff1e7a3f6fec9b55ef
rolexardon <rolando_ardon299@hotmail.com>
2014-10-09 15:30:13 -0600

    'clientes_admin'

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from clientes.models import cliente
+from clientes.models import cliente,saldo_cliente
 
 admin.site.register(cliente)
+admin.site.register(saldo_cliente)
+
